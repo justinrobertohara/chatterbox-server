f7094f9e3db4d58e5f1b1d419fb80f31ff643247
Justin O'Hara <justinrobertohara@gmail.com>
2019-06-12 17:17:48 -0700

    wednesday progress

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -1,18 +1,17 @@
 var Parse = {
-
-  server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
+  server: `http://127.0.0.1:3000/`,
 
   create: function(message, successCB, errorCB = null) {
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
-      url: Parse.server,
+      url: Parse.server + 'classes/messages',
       type: 'POST',
       data: JSON.stringify(message),
       contentType: 'application/json',
-      success: function (data) {
+      success: function(data) {
         console.log('chatterbox: Message sent');
       },
-      error: function (data) {
+      error: function(data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message', data);
       }
@@ -21,15 +20,16 @@ var Parse = {
 
   readAll: function(successCB, errorCB = null) {
     $.ajax({
-      url: Parse.server,
+      url: Parse.server + 'classes/messages',
       type: 'GET',
-      data: { order: '-createdAt' },
+      //data: { order: '-createdAt' },
       contentType: 'application/json',
       success: successCB,
-      error: errorCB || function(error) {
-        console.error('chatterbox: Failed to fetch messages', error);
-      }
+      error:
+        errorCB ||
+        function(error) {
+          console.error('chatterbox: Failed to fetch messages', error);
+        }
     });
   }
-
 };
